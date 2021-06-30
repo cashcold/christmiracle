5914b2cc40dbc8242f920cfa8b30964fd5912ca8
cashcold <cashcold99@gmail.com>
2021-06-30 08:30:23 +0000

    updating sli

--- client/src/Project-cOMPONENT/Slibebar/slibebar.js
+++ client/src/Project-cOMPONENT/Slibebar/slibebar.js
@@ -19,7 +19,16 @@ class SlibebarMain extends Component {
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
+                        <img className="d-block w-100" src={require('../../pic/IMG-20200120-WA0006.jpg')} />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img className="d-block w-100" src={require('../../pic/Restored/IMG_4870.jpg')} />
+                    </Carousel.Item>
+                    <Carousel.Item>
                         <img className="d-block w-100" src={require('../../pic/Restored/IMG_20210404_125719443.jpg')} />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img className="d-block w-100" src={require('../../pic/Restored/IMG_4698.jpg')} />
                     </Carousel.Item>
                     <Carousel.Item>
                         <img className="d-block w-100" src={require('../../pic/IMG_20200926_124853.jpg')} />
@@ -38,6 +47,9 @@ class SlibebarMain extends Component {
                     </Carousel.Item>
                     <Carousel.Item>
                         <img className="d-block w-100" src={require('../../pic/Restored/IMG_20210328_085248569.jpg')} />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img className="d-block w-100" src={require('../../pic/Restored/IMG_4665.jpg')} />
                     </Carousel.Item>
                     
                     <Carousel.Item>
