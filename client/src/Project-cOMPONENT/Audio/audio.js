cb1806dcdbe570cf8cd5ad67c31d51aac568d67c
cashcold <cashcold99@gmail.com>
2022-02-07 08:17:03 +0000

    update on audio

--- client/src/Project-cOMPONENT/Audio/audio.js
+++ client/src/Project-cOMPONENT/Audio/audio.js
@@ -8,10 +8,23 @@ class AudioMain extends Component {
     render() { 
         return ( 
             <div classNam='mainAudio'>
-                <section className='audioNow'>
-                    <div className='newAudio'>
-                        <h1>Audio coming soon.................</h1>
-                    </div>
+                 <section className='audioNow_layOut_1'>
+                    <iframe  src="https://open.spotify.com/embed/playlist/37i9dQZF1DX02V1eCHz2oO?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+                </section>
+                <section className='audioNow_layOut_1'>
+                    <iframe  src="https://open.spotify.com/embed/playlist/37i9dQZF1DX3RSrobAVgId?utm_source=generator" width="100%" height="700" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+                </section>
+                <section className='audioNow_layOut_1'>
+                <iframe  src="https://open.spotify.com/embed/album/3dY5SHT5Tx1oXIIUE9zoGH?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+                </section>
+                <section className='audioNow_layOut_1'>
+                    <iframe  src="https://open.spotify.com/embed/playlist/2tkwlVDrj0EymokWhtR3lw?utm_source=generator" width="100%" height="700" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+                </section>
+                <section className='audioNow_layOut_1'>
+                    <iframe  src="https://open.spotify.com/embed/playlist/2rdcocOpQzLq0VI0lmGOAb?utm_source=generator" width="100%" height="700" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+                </section>
+                <section className='audioNow_layOut_1'>
+                    <iframe  src="https://open.spotify.com/embed/playlist/7cjwCLfip6gVrf5fSgdWOE?utm_source=generator" width="100%" height="700" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
                 </section>
             </div>
          );
