bb88105b183bdd026cef78467e97149443c63723
Frank99 <cashcold99@gmail.com>
2023-01-21 08:58:37 +0000

    update from new machine

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom' 
 import NavbarMain from './Project-cOMPONENT/Navbar/navbar';
 import HomeMain from './Project-cOMPONENT/Home/home';
 import AimMain from './Project-cOMPONENT/Aims/aim';
--- server.js
+++ server.js
@@ -25,7 +25,7 @@ if(process.env.NODE_ENV === 'production'){
     JungleServer.use(express.static("client/build"))
     JungleServer.get('*',(req,res)=>{
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
+    }) 
 }
 
 
