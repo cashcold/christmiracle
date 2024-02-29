5ea570f1efe222fd91497f81a4436721e5a1b889
Frank99 <cashcold99@gmail.com>
2024-02-29 11:04:58 +0000

    final getting social preview

--- server.js
+++ server.js
@@ -19,17 +19,10 @@ const app = express()
 
 app.use(cors())
 app.use(bodyParser.json())
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+// app.use(express.static(path.join(__dirname, 'client', 'build')));
  
 app.use('/users',userRouter)
 
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname, 'client', 'build')));
-    // app.use(express.static("client/build"))
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    }) 
-}
 
 
 app.get('/', function(request, response) {
@@ -98,6 +91,16 @@ app.get('/christains_books/:id', function(request, response) {
 
 });
 
+
+
+app.use(express.static("client/build"))
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static("client/build"))
+  app.get('*',(req,res)=>{
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+
+}
 
 app.listen(PORT,()=>{
     console.log(`server is runing on local Port Number ${PORT}`)
