799d3d5d260faa9a8310df37fc34fe01dc544511
Frank99 <cashcold99@gmail.com>
2024-10-02 00:11:26 +0000

    update on resouces

---
+++ client/src/Project-cOMPONENT/API/resources_api.js
@@ -0,0 +1,30 @@
+import moment from 'moment'
+
+export const resources_api = [
+    {
+        id: 1,
+        head_Text_url: 'Angels-Seen-Hovering-Over-Jerusalem-–-A-Divine-Sign-from-God!',
+        name: 'Angels Seen Hovering Over Jerusalem – A Divine Sign from God!',
+        title: `Angels Seen Hovering Over Jerusalem – A Divine Sign from God!\n\n `,
+        description: `Dear Beloved in Christ,\n\n
+        We are witnessing something truly miraculous! Angels have been seen hovering over the holy city of Jerusalem—a sign that is filling believers worldwide with awe and wonder. This divine occurrence serves as a powerful reminder of God's continued presence and protection over His people.`,
+        Chapter_1: `As we reflect on this heavenly manifestation, let us be reminded that God’s messengers are always near, ministering to us, guiding us, and watching over the nations. This event brings encouragement to all who believe in the promises of the Lord and His divine plan for Jerusalem and the world.`,
+        Chapter_2: `Let this be a call for all of us to draw nearer to God in prayer, faith, and holiness. The sight of these angels is not just a miracle to behold, but a confirmation that God is preparing His people and His church for greater things.`,
+        Chapter_3: `Stay connected in faith, and may we all find hope in this heavenly sign!`,
+        Chapter_4: ``,
+        Chapter_5: ``,
+        Chapter_6: ``,
+        Chapter_7: ``,
+        Chapter_Conclusion: ``,
+        Chapter_Info: `Angels-Seen-Hovering-Over-Jerusalem–-A-Divine-Sign-from-God`,
+        date:  moment('2024-01-01T07:30:00.000Z').format('LLLL'),
+        eye_seen: '847.9k',
+        social_on_image: `https://firebasestorage.googleapis.com/v0/b/thechristmiracles.appspot.com/o/images%2FWhy-Given-Thanks-Is-Important.jpg?alt=media&token=c0de8daf-9bc7-4ea2-93aa-24c57b538e11`,
+        social_on_image_label: `https://firebasestorage.googleapis.com/v0/b/thechristmiracles.appspot.com/o/images%2FWhy-Given-Thanks-Is-Important.jpg?alt=media&token=c0de8daf-9bc7-4ea2-93aa-24c57b538e11`,
+    }
+   
+ 
+    
+ 
+
+]
