1e73083de8feeb8b2895a38e587563db40559c81
Frank99 <cashcold99@gmail.com>
2024-10-11 02:42:52 -0700

    update on netify host

--- client/src/Project-cOMPONENT/API/resources_api.js
+++ client/src/Project-cOMPONENT/API/resources_api.js
@@ -4,7 +4,7 @@ export const resources_api = [
     {
         id: 1,
         head_Text_url: 'Angels-Seen-Hovering-Over-Jerusalem-–-A-Divine-Sign-from-God!',
-        name: 'Angels Seen Hovering Over Jerusalem – A Divine Sign from God!',
+        name: 'The Miraculous Team',
         title: `Angels Seen Hovering Over Jerusalem – A Divine Sign from God!\n\n `,
         description: `Dear Beloved in Christ,\n\n
         We are witnessing something truly miraculous! Angels have been seen hovering over the holy city of Jerusalem—a sign that is filling believers worldwide with awe and wonder. This divine occurrence serves as a powerful reminder of God's continued presence and protection over His people.`,
@@ -19,8 +19,8 @@ export const resources_api = [
         Chapter_Info: `Angels-Seen-Hovering-Over-Jerusalem–-A-Divine-Sign-from-God`,
         date:  moment('2024-01-01T07:30:00.000Z').format('LLLL'),
         eye_seen: '847.9k',
-        social_on_image: `https://firebasestorage.googleapis.com/v0/b/thechristmiracles.appspot.com/o/images%2FWhy-Given-Thanks-Is-Important.jpg?alt=media&token=c0de8daf-9bc7-4ea2-93aa-24c57b538e11`,
-        social_on_image_label: `https://firebasestorage.googleapis.com/v0/b/thechristmiracles.appspot.com/o/images%2FWhy-Given-Thanks-Is-Important.jpg?alt=media&token=c0de8daf-9bc7-4ea2-93aa-24c57b538e11`,
+        social_on_image: `https://firebasestorage.googleapis.com/v0/b/the-christ-d3d67.appspot.com/o/images%2F2.jpg?alt=media&token=0a7ef146-738f-4caf-a112-7921873d47d3`,
+        social_on_image_label: `https://firebasestorage.googleapis.com/v0/b/the-christ-d3d67.appspot.com/o/images%2F2.jpg?alt=media&token=0a7ef146-738f-4caf-a112-7921873d47d3`,
     }
    
  
--- client/src/Project-cOMPONENT/Resources/Resources.js
+++ client/src/Project-cOMPONENT/Resources/Resources.js
@@ -1,31 +1,93 @@
 import React, { Component } from 'react';
-import {resources_api } from '../API/resources_api'
-import moment from 'moment'
+import moment from 'moment';
 import { Helmet } from 'react-helmet';
-import './Resources.css'
+import { resources_api } from '../API/resources_api';  // Using resources_api now
+import './Resources.css';
 
 class ResourcesMain extends Component {
     constructor(props) {
         super(props);
-        this.state = { 
-            resources_api: resources_api,
-         }
+        this.state = {
+            resources_api: resources_api  // Updated to resources_api
+        };
     }
-    componentDidMount(){
-        console.log(this.state.resources_api)
 
-
+    componentDidMount() {
+        console.log(this.state.resources_api);
     }
-    render() { 
 
-        console.log(this.state.resources_api)
-        
-        return ( 
-            <div classNam='ResourcesMain'>
-                 <h1>Resources Main</h1>
+    render() {
+        return (
+            <div className='mainMessage'>
+                <Helmet>
+                    <base />
+                    <title>Rosources</title>
+                    <meta name="description" content="Read about the miraculous event " />
+                    <link rel="canonical" href="somelink" />
+                </Helmet>
+                
+                <section className='motivation_text'>
+                    <h3>The End Time</h3>
+                </section>
+
+                <section className='messageNow'>
+                    <div className='newMessage newMessage_box_1'>
+                        <h2>Event Highlights</h2>
+                        <div className='display_message'>
+                            {this.state.resources_api.map(data => (
+                                <div className='message_flow_home' key={data.id}>
+                                    <h4 dangerouslySetInnerHTML={{ __html: data.title.replace(/\n/g, '<br />') }}></h4>
+                                    <img className='social_img' src={data.social_on_image} alt={data.name} />
+
+                                    <div className='icon_star'>
+                                        {[...Array(5)].map((_, index) => (
+                                            <i key={index} className="fas fa-star"></i>
+                                        ))}
+                                    </div>
+
+                                    <div className='viewed_tag'>
+                                        <span><i className="fa-solid fa-eye"></i> Viewed {data.eye_seen}</span>
+                                    </div>
+
+                                    <p>By {data.name}</p>
+                                    <span><i className="fa fa-clock-o"></i> <span>{moment(data.date).format('LLLL')}</span></span>
+                                    
+                                    <h4 className='desc_text'>{data.description}</h4>
+
+                                    <button
+                                        onClick={() => {
+                                            localStorage.setItem('resources_api_id', data.id);
+                                            localStorage.setItem('resources_api_name', data.name);
+                                            localStorage.setItem('resources_api_title', data.title);
+                                            localStorage.setItem('social_on_image', data.social_on_image);
+
+                                            const paramsUrl = {
+                                                head_Text_url: data.head_Text_url,
+                                                Chapter_Info: data.Chapter_Info,
+                                                social_on_image: data.social_on_image
+                                            };
+
+                                            const queryParams = require('query-string');
+                                            const passParams = queryParams.stringify(paramsUrl);
+
+                                            window.location = `/christains_resources/${data.id}?${passParams}`;
+                                        }}
+                                        className='btn btn-warning btn_readMore'>
+                                        Read More &#8594;
+                                    </button>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                    <div className='newMessage newMessage_box_2'>
+                        <h4>Find Out More About Missions and Evangelism</h4>
+                        <button className='btn btn-warning'>Find Out</button>
+                        <hr className='underline_line' />
+                    </div>
+                </section>
             </div>
-         );
+        );
     }
 }
- 
+
 export default ResourcesMain;
