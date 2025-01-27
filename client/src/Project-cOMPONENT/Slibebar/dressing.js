ab86c3969ddf99733e1d589a2374e12314adcde3
Frank99 <cashcold99@gmail.com>
2025-01-27 20:58:00 -0100

    update on slibar images

--- client/src/Project-cOMPONENT/Slibebar/dressing.js
+++ client/src/Project-cOMPONENT/Slibebar/dressing.js
@@ -12,20 +12,8 @@ class Dressing extends Component {
             <section className='carl'>
                 <h1> CHRISTIAN DRESSING</h1>
                     <Carousel fade>
-                        <Carousel.Item>
-                            <img className="d-block w-100" src={require('../../pic/New folder/IMG_20210530_143248310.jpg')} />
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img className="d-block w-100" src={require('../../pic/New folder (4)/IMG_20210808_143123059.jpg')} />
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img className="d-block w-100" src={require('../../pic/New folder (4)/IMG_20210808_142637116.jpg')} />
-                        </Carousel.Item>
-                        <Carousel.Item>
+                       <Carousel.Item>
                             <img className="d-block w-100" src={require('../../pic/New folder (4)/IMG_20210808_143343370.jpg')} />
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img className="d-block w-100" src={require('../../pic/New folder (4)/IMG_20210808_142552898.jpg')} />
                         </Carousel.Item>
                         <Carousel.Item>
                             <img className="d-block w-100" src={require('../../pic/New folder (4)/IMG_20210808_143233624.jpg')} />
@@ -39,12 +27,7 @@ class Dressing extends Component {
                                 <h3>FOR COUPLE</h3>
                             </Carousel.Caption>
                         </Carousel.Item>
-                        <Carousel.Item>
-                            <img className="d-block w-100" src={require('../../pic/New folder (4)/IMG_20210808_143740171.jpg')} />
-                            <Carousel.Caption>
-                                <h3>FOR COUPLE</h3>
-                            </Carousel.Caption>
-                        </Carousel.Item>
+                        
                         <Carousel.Item>
                             <img className="d-block w-100" src={require('../../pic/New folder/IMG_20201227_140321224_HDR.jpg')} />
                         </Carousel.Item>
